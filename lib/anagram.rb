9cc63606328797cf9fe48f5a8523fcbd26cd976e
Austin  Burke <aburk3@gmail.com>
2018-10-09 21:50:40 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,33 +1,33 @@
-# Your code goes here!
-require 'pry'
-class Anagram
+# # Your code goes here!
+# require 'pry'
+# class Anagram
   
-  attr_accessor :word, :word_array_sorted, :some_word_array, :match_array
+#   attr_accessor :word, :word_array_sorted, :some_word_array, :match_array
   
-  def initialize(word)
-    @word = word 
-  end
+#   def initialize(word)
+#     @word = word 
+#   end
   
-  def check_for_match(possible_anagrams)
-    some_word_array = []
+#   def check_for_match(possible_anagrams)
+#     some_word_array = []
     
-    possible_anagrams.detect do |some_word|
-      some_word_array.clear
-      some_word_array = some_word.split("").sort!
-      # binding.pry
-      @word_array_sorted = @word.split("").sort!
-      @some_word_array == word_array_sorted
-      # binding.pry
-    end
-  end
+#     possible_anagrams.detect do |some_word|
+#       some_word_array.clear
+#       some_word_array = some_word.split("").sort!
+#       # binding.pry
+#       @word_array_sorted = @word.split("").sort!
+#       @some_word_array == word_array_sorted
+#       # binding.pry
+#     end
+#   end
   
-  def match(possible_anagrams)
-    @match_array = []
-    if check_for_match(possible_anagrams)
-        @match_array << @some_word_array
-    else
-        @match_array
-    end
-  end
+#   def match(possible_anagrams)
+#     @match_array = []
+#     if check_for_match(possible_anagrams)
+#         @match_array << @some_word_array
+#     else
+#         @match_array
+#     end
+#   end
   
-end
+# end
