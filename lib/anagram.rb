171262e06afe66ef55e87434305c7fcf5350eb2d
Austin  Burke <aburk3@gmail.com>
2018-10-09 20:36:36 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,13 +13,14 @@ class Anagram
     some_word_array = []
     match_array = []
     
-    match_array << possible_anagrams.detect do |some_word|
+    if possible_anagrams.detect do |some_word|
       some_word_array.clear
       some_word_array = some_word.split("").sort!
       # binding.p ry
       word_array_sorted = @word.split("").sort
       some_word_array == word_array_sorted
       # binding.pry
+      
     end
     
     match_array
