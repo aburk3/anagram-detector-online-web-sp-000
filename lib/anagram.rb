f387ad93a76ee962c6545fa4993328de526faec5
Austin  Burke <aburk3@gmail.com>
2018-10-09 20:33:49 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+    
+  end
+  
+  def match(possible_anagrams)
+    some_word_array = []
+    match_array = []
+    
+    match_array << possible_anagrams.detect do |some_word|
+      some_word_array.clear
+      some_word_array = some_word.split("").sort!
+      # binding.p ry
+      word_array_sorted = @word.split("").sort
+      some_word_array == word_array_sorted
+      # binding.pry
+    end
+    
+    match_array
+      
+      
+  end
+  
+end
