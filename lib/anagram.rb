31cc97429075337c576fb9810572a6b1ce29b8bb
Austin  Burke <aburk3@gmail.com>
2018-10-09 21:50:43 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -31,3 +31,17 @@
 #   end
   
 # end
+
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word 
+  end   
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
